8314690c70a1376f7d1310c338ea55f43c680ceb
Bridger Tower <bridgertower@gmail.com>
2024-05-23 22:35:07 -0400

    update homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Space_Grotesk as FontSans } from "next/font/google";
+import { Section, Container } from "@/components/craft";
 import { ThemeProvider } from "@/components/theme/theme-provider";
+import Logo from "@/public/logo.svg";
+import Balancer from "react-wrap-balancer";
 
 import "./globals.css";
 
@@ -46,6 +49,7 @@ export default function RootLayout({
         >
           <Nav />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
@@ -91,5 +95,43 @@ const Nav = ({ className, children, id }: NavProps) => {
         </div>
       </div>
     </nav>
+  );
+};
+
+const Footer = () => {
+  return (
+    <footer className="not-prose border-t">
+      <Section>
+        <Container className="grid gap-6">
+          <div className="grid gap-6">
+            <Link href="/">
+              <h3 className="sr-only">brijr/components</h3>
+              <Image
+                src={Logo}
+                alt="Logo"
+                width={120}
+                height={27.27}
+                className="dark:invert hover:opacity-75 transition-all"
+              ></Image>
+            </Link>
+            <p>
+              <Balancer>
+                We built Router.so to help developers build forms faster and
+                capture leads easier.
+              </Balancer>
+            </p>
+            {/* <div className="flex flex-col md:flex-row mb-6 md:mb-0 gap-4 underline underline-offset-4 text-sm text-muted-foreground">
+              <Link href="/privacy-policy">Privacy Policy</Link>
+              <Link href="/terms-of-service">Terms of Service</Link>
+              <Link href="/cookie-policy">Cookie Policy</Link>
+            </div> */}
+            <p className="text-muted-foreground">
+              © <a href="https://9d8.dev">9d8</a>. All rights reserved.
+              2024-present.
+            </p>
+          </div>
+        </Container>
+      </Section>
+    </footer>
   );
 };
